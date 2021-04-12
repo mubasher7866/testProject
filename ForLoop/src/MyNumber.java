5373c1da5989ca92dc47aff55b907dd88f65765f
Mubasher <21694@student.dorset-college.ie>
2021-04-12 15:49:18 +0100

    commit

---
+++ ForLoop/src/MyNumber.java
@@ -0,0 +1,61 @@
+
+public class MyNumber {
+	
+	private int number;
+
+    public MyNumber(int number) {
+        this.number = number;
+    }
+
+    public boolean isPrime() {
+        
+        for(int i = 2; i <= number - 1 ; i++){
+        if (number % 2 == 0){
+            return false;
+        }
+    }
+        return true;
+
+    }
+
+              public int sumUpToN() {
+                  
+                  int sum = 0;
+                  
+                  for( int i = 1; i <= number; i++){
+                      sum = sum + i;
+                  }
+                  return sum;
+                  
+        }
+
+    public int sumOfDivisors() {
+        
+        int sum = 0;
+        
+        for(int i = 2; i < number; i++){
+            if(number% i ==0){
+            sum = sum + i;
+        }
+      
+        }
+      return sum;
+ }
+
+    public void printNumberTriangle() {
+        
+        for(int i = 1 ; i <=number; i++){
+            for(int j = 1 ; j <= i ; j++){
+            System.out.print(j + " ");
+        }
+            System.out.println();
+        }
+        
+        
+    }
+ }
+
+
+	
+
+
